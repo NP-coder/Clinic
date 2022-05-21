158b3ef5c53d666c981f53aab76701896fb6cdf1
Nazar Plahtiy <nazarplahtij@gmail.com>
2022-05-21 12:49:55 +0300

    Добавьте файлы проекта.

---
+++ Clinic/Migrations/20220405181744_Rework.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Clinic.Migrations
+{
+    public partial class Rework : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Approved",
+                table: "Appointments",
+                newName: "IsDoctorApproved");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsDoctorApproved",
+                table: "Appointments",
+                newName: "Approved");
+        }
+    }
+}
---
+++ Clinic/Teammate/EmailSender.cs
@@ -0,0 +1,38 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clinic.Teammate
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailjetClient client = new MailjetClient("42b785ecc2166e99f742e57881ef8a20","30be3c592d51f4323ae8438f09049507")
+            {
+                
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+            .Property(Send.FromEmail, "nazarplahtij@gmail.com")
+            .Property(Send.FromName, "Your Clinic")
+            .Property(Send.Subject, subject)
+            .Property(Send.HtmlPart, htmlMessage)
+            .Property(Send.Recipients, new JArray {
+                new JObject {
+                 {"Email", email}
+                 }
+                });
+
+            MailjetResponse response = await client.PostAsync(request);
+             
+        }
+    }
+}
